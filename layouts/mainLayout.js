5657879886fd3379daddf2bc3d05c7d49828c27d
salahED <salah.tebib93@gmail.com>
2020-12-13 20:05:25 +0100

    Adding some assets ✨

---
+++ layouts/mainLayout.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import Screen from "./Screen";
+
+export default function MainLayout() {
+  return (
+    <Screen>
+      <View></View>
+    </Screen>
+  );
+}
